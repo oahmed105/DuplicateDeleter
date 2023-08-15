a6a7a662319c3628b90627c269362651da6986e6
oahmed105 <oahmed10598@gmail.com>
2023-08-15 14:51:10 -0400

    making progress

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,26 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    private Integer[] intArray;
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] intArr = new Integer[intArray.length - maxNumberOfDuplications];
+        Integer[] newArr = new Integer[intArr.length];
+
+        for (int i = 0; i < newArr.length; i++){
+            if (i != maxNumberOfDuplications){
+                newArr[i] = i;
+            }
+        }
+        return newArr;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return new Integer[0];
+    }
 }
